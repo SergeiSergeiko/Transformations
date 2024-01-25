9fafce1f2cb217b7264749e25e779a318a0fc9d2
SergeiSergeiko <81086218+SergeiSergeiko@users.noreply.github.com>
2024-01-25 16:15:51 +0100

    bug fixes January 19, 2024

---
+++ Assets/Scripts/Resizer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Resizer : MonoBehaviour
+{
+    [SerializeField] float _speed;
+
+    private void Update()
+    {
+        float newSize = _speed * Time.deltaTime;
+
+        transform.localScale += Vector3.one * newSize;
+    }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+        
+    private void Update()
+    {
+        float rotationY = _speed * Time.deltaTime;
+        Vector3 eulers = new(0, rotationY, 0);
+
+        transform.Rotate(eulers);
+    }
+}
