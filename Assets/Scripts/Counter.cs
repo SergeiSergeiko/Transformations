a5fffeae96135c5e44d632d81abf62d99cb1872f
SergeiSergeiko <81086218+SergeiSergeiko@users.noreply.github.com>
2024-05-11 23:56:02 +0200

    Counter

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(TextViewer))]
+public class Counter : MonoBehaviour
+{
+    private float _count;
+    private float _numberIncrease = 1;
+    private float _delay = 0.5f;
+    private bool _isActive = false;
+
+    public float Count
+    {
+        get => _count;
+
+        private set
+        {
+            _count += value;
+            Changed?.Invoke();
+        }
+    }
+
+    public UnityAction Changed;
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    private void Start()
+    {
+        Count = 0;
+        StartCoroutine(CountIncrease());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            _isActive = !_isActive;
+    }
+
+    private IEnumerator CountIncrease()
+    {
+        bool isOpen = true;
+
+        while (isOpen)
+        {
+            yield return new WaitUntil(() => _isActive);
+            yield return new WaitForSeconds(_delay);
+
+            Count = _numberIncrease;
+        }
+    }
+}
---
+++ Assets/Scripts/TextViewer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class TextViewer : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _text;
+
+    private Counter _counter;
+
+    private void OnEnable()
+    {
+        _counter = GetComponent<Counter>();
+        _counter.Changed += ViewText;
+    }
+
+    private void OnDisable()
+    {
+        _counter.Changed -= ViewText;
+    }
+
+    private void ViewText()
+    {
+        _text.text = _counter.Count.ToString();
+    }
+}
