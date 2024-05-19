c0ca9d30467bb541f497cf23b35f07c4b2b20e73
SergeiSergeiko <81086218+SergeiSergeiko@users.noreply.github.com>
2024-05-19 20:22:51 +0200

    Сube explosions

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class Cube : MonoBehaviour
+{
+    [SerializeField] private float _explosionForce;
+    [SerializeField] private float _explosionRadius;
+    [SerializeField] private ParticleSystem _dieEffect;
+
+    private Renderer _renderer;
+
+    public event Action<Cube, Vector3> Destroying;
+
+    private void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+
+        ChangeColorRandomly();
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy();
+    }
+
+    private void Destroy()
+    {
+        Destroying?.Invoke(this, transform.localScale);
+        Explode();
+        StartDieEffect();
+        Destroy(gameObject);
+    }
+
+    private void StartDieEffect()
+    {
+        ChangeColorEffect(_dieEffect, _renderer.material.color);
+        Instantiate(_dieEffect, transform.position, Quaternion.identity);
+    }
+
+    private void ChangeColorRandomly()
+    {
+        float red = UnityEngine.Random.Range(0f, 1f);
+        float green = UnityEngine.Random.Range(0f, 1f);
+        float blue = UnityEngine.Random.Range(0f, 1f);
+
+        _renderer.material.color = new Color(red, green, blue);
+    }
+
+    private void ChangeColorEffect(ParticleSystem particleSystem, Color color)
+    {
+        ParticleSystem.MainModule mainModule = particleSystem.main;
+        mainModule.startColor = color;
+    }
+
+    private void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, _explosionRadius);
+
+        foreach (var collider in colliders)
+            collider.attachedRigidbody?.AddExplosionForce(_explosionForce,
+                transform.position, _explosionRadius);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Cube _prefab;
+    [SerializeField] private Transform _spawnPoint;
+    [SerializeField] private GameLogic _gameLogic;
+
+    private int _startCountCubes = 3;
+
+    private void Start()
+    {
+        Spawn(_spawnPoint, _prefab.transform.localScale, _startCountCubes);
+    }
+
+    public void Spawn(Transform transform, Vector3 scale, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Cube newObject = Instantiate(_prefab, transform.position, Quaternion.identity);
+            newObject.transform.localScale = scale;
+            newObject.Destroying += _gameLogic.Method;
+        }
+    }
+}
